85dddbc4bc4c834f50352290db908b329f164049
Noamshalev1 <113979512+Noamshalev1@users.noreply.github.com>
2023-05-28 20:24:52 +0300

    Let's getting started : )

--- SharableSpreadSheet/Form1.cs
+++ SharableSpreadSheet/Form1.cs
@@ -25,9 +25,46 @@ namespace SharableSpreadSheet
         {
             viewer.Rows[row].Cells[col].Value = info;
         }
-        
+
+        public void SwitchRows(int row1, int row2)
+        {
+            for(int i=0; i<viewer.Columns.Count; i++) 
+            {
+                DataGridViewCell tmp = viewer.Rows[row1].Cells[i];
+                viewer.Rows[row1].Cells[i].Value = viewer.Rows[row2].Cells[i].Value;
+                viewer.Rows[row2].Cells[i].Value = tmp.Value;
+            }
+        }
+
+        public void SwitchCols(int col1, int col2)
+        {
+            for (int i = 0; i < viewer.Rows.Count; i++)
+            {
+                DataGridViewCell tmp = viewer.Rows[i].Cells[col1];
+                viewer.Rows[i].Cells[col1].Value = viewer.Rows[i].Cells[col2].Value;
+                viewer.Rows[i].Cells[col2].Value = tmp.Value;
+            }
+        }
+
+        public void SearchInRow(int row, String str, out int col)
+        {
+            col = -1;
+            for(int i = 0; i<viewer.Columns.Count;i++)
+                if (viewer.Rows[row].Cells[i].Value.Equals(str))
+                    col = i; 
+        }
+
+        public void SearchInCol(int col, String str, out int row)
+        {
+            row = -1;
+            for (int i = 0; i < viewer.Rows.Count; i++)
+                if (viewer.Rows[i].Cells[col].Value.Equals(str))
+                    row = i;
+        }
+
         public void SearchInViewer(String str, out int row, out int col) 
         {
+            row = 0; col = 0;
             for(int i=0; i<viewer.Rows.Count; i++)
             {
                 for(int j=0; j < viewer.Columns.Count; j++) 
@@ -38,6 +75,50 @@ namespace SharableSpreadSheet
                     }
                 }
             }
+        }
+
+        public void AddRow(int row)
+        {
+            DataGridViewRow newRow = new DataGridViewRow();
+            for(int i=0; i<viewer.Columns.Count; i++)
+                newRow.Cells.Add(new DataGridViewTextBoxCell());
+            viewer.Rows.Insert(row+1, newRow);
+        }
+
+        public void AddCol(int col)
+        {
+            DataGridViewColumn newCol = new DataGridViewColumn();
+            viewer.Columns.Insert(col+1, newCol);
+        }
+
+        public List<Tuple<int, int>> FindAll(String str, bool caseSensitive)
+        {
+            List<Tuple<int, int>> matchingCells = new List<Tuple<int, int>>();
+
+            // Iterate over each row in the DataGridView
+            foreach (DataGridViewRow row in viewer.Rows)
+            {
+                // Iterate over each cell in the row
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    // Perform the search based on case sensitivity flag
+                    bool isMatch;
+                    if (caseSensitive)
+                        isMatch = cell.Value?.ToString().Contains(str) == true;
+                    else
+                        isMatch = cell.Value?.ToString().IndexOf(str, StringComparison.OrdinalIgnoreCase) >= 0;
+
+                    // If the cell value matches, add the cell coordinates to the list
+                    if (isMatch)
+                    {
+                        int rowIndex = cell.RowIndex;
+                        int columnIndex = cell.ColumnIndex;
+                        matchingCells.Add(new Tuple<int, int>(rowIndex, columnIndex));
+                    }
+                }
+            }
+
+            return matchingCells;
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
--- SharableSpreadSheet/SharableSpreadSheet.cs
+++ SharableSpreadSheet/SharableSpreadSheet.cs
@@ -1,15 +1,20 @@
 ﻿using System;
+using System.Windows.Forms;
 
 namespace SharableSpreadSheet
 {
     class SharableSpreadSheet
     {
         Form1 dataGrid;
+        int nR;
+        int nC;
         public SharableSpreadSheet(int nRows, int nCols, int nUsers=-1)
         {
             // nUsers used for setConcurrentSearchLimit, -1 mean no limit.
             // construct a nRows*nCols spreadsheet
             dataGrid = new Form1();
+            nR = nRows; 
+            nC = nCols;
             dataGrid.SetViewer(0, 0, nCols, nRows);
                
         }
@@ -34,62 +39,111 @@ namespace SharableSpreadSheet
         public void exchangeRows(int row1, int row2)
         {
             // exchange the content of row1 and row2
+            dataGrid.SwitchRows(row1, row2);
         }
         public void exchangeCols(int col1, int col2)
         {
             // exchange the content of col1 and col2
+            dataGrid.SwitchCols(col1, col2);
         }
         public int searchInRow(int row, String str)
         {
             int col;
             // perform search in specific row
+            dataGrid.SearchInRow(row, str, out col);
             return col;
         }
         public int searchInCol(int col, String str)
         {
             int row;
             // perform search in specific col
+            dataGrid.SearchInCol(col, str, out row);
             return row;
         }
         public Tuple<int, int> searchInRange(int col1, int col2, int row1, int row2, String str)
         {
-            int row,col
             // perform search within spesific range: [row1:row2,col1:col2] 
             //includes col1,col2,row1,row2
-            return <row,col>;
+            for(int i = col1; i <= col2; i++)
+            {
+                for(int j = row1; j <= row2; j++)
+                {
+                    if (dataGrid.GetCellInViewer(j, i).Equals(str))
+                        return Tuple.Create(i, j);
+                }
+            }
+            return Tuple.Create(-1, -1);
         }
         public void addRow(int row1)
         {
-            //add a row after row1
+            dataGrid.AddRow(row1);
+            nR++;
         }
         public void addCol(int col1)
         {
             //add a column after col1
+            dataGrid.AddCol(col1);
+            nC++;
         }
         public Tuple<int, int>[] findAll(String str,bool caseSensitive)
         {
-            // perform search and return all relevant cells according to caseSensitive param
+            List<Tuple<int, int>> matchingCells = dataGrid.FindAll(str, caseSensitive);
+            return matchingCells.ToArray();
         }
-        public void setAll(String oldStr, String newStr bool caseSensitive)
+        public void setAll(String oldStr, String newStr ,bool caseSensitive)
         {
             // replace all oldStr cells with the newStr str according to caseSensitive param
+            Tuple<int, int>[] cellsToSet = findAll(oldStr, caseSensitive);
+            foreach(Tuple<int,int> cell in cellsToSet)
+                dataGrid.SetCellInViewer(cell.Item1, cell.Item2, newStr);
         }
         public Tuple<int, int> getSize()
         {
-            int nRows, int nCols;
             // return the size of the spreadsheet in nRows, nCols
-            return<nRows,nCols>;
+            return Tuple.Create(nR, nC);
         }
 
         public void save(String fileName)
         {
-            // save the spreadsheet to a file fileName.
-            // you can decide the format you save the data. There are several options.
+            using (FileStream fileStream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            using (StreamWriter writer = new StreamWriter(fileStream))
+            {
+                for (int row = 0; row < nR; row++)
+                {
+                    for (int col = 0; col < nC; col++)
+                    {
+                        string cellValue = dataGrid.GetCellInViewer(row, col); // Replace with your own method to retrieve cell value
+                        writer.Write(cellValue);
+                        if (col < nC - 1)
+                        {
+                            writer.Write(",");
+                        }
+                    }
+                    writer.WriteLine();
+                }
+            }
         }
         public void load(String fileName)
         {
-            // load the spreadsheet from fileName
-            // replace the data and size of the current spreadsheet with the loaded data
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                string line;
+                int row = 0;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] cellValues = line.Split(',');
+                    int col = 0;
+
+                    foreach (string cellValue in cellValues)
+                    {
+                        dataGrid.SetCellInViewer(row, col, cellValue.Trim()); // Replace with your own method to set the cell value
+                        col++;
+                    }
+
+                    row++;
+                }
+            }
         }
     }
 }
